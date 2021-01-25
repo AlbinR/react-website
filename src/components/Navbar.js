cde0c91c4b0247f5c81d0eef5f9412e111b8c2f7
AlbinR <albin@rybergs.net>
2021-01-25 16:40:13 +0100

    23.06

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,39 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        Albin Ryberg
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/" className='navbar-logo'>
+                        Albin Ryberg <i className='fab fa-typo3'></i>
                     </Link>
-                    <div className='menuicon'>
+                    <div className='menuicon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-link' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-link' onClick={closeMobileMenu}>
+                                Work
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-link' onClick={closeMobileMenu}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
